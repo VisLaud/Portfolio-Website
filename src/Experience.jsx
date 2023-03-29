794f1c357605d5525159ad40ea189926be53e2ff
Simanta Thapa <44049186+VisLaud@users.noreply.github.com>
2023-03-28 22:41:11 -0400

    added iframe to include the portfolio website

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,6 +3,9 @@ import {
   Float,
   useGLTF,
   PresentationControls,
+  ContactShadows,
+  Html,
+  Text,
 } from '@react-three/drei';
 
 export default function Experience() {
@@ -15,11 +18,45 @@ export default function Experience() {
 
       <color args={['#695b5b']} attach="background" />
 
-      <PresentationControls global>
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
         <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={'#ff6900'}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
           <primitive object={laptop.scene} position-y={-1.2} />
+          <Html
+            transform
+            wrapperClass="htmlScreen"
+            distanceFactor={1.17}
+            position={[0, 0.33, -1.4]}
+            rotation-x={-0.25}
+          >
+            <iframe src="https://simantathapa.netlify.app/" />
+          </Html>
+          <Text
+            font="./bangers-v20-latin-regular.woff"
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+          >
+            Simanta Thapa
+          </Text>
         </Float>
       </PresentationControls>
+      <ContactShadows position-y={-1.5} opacity={0.4} scale={5} blur={2.4} />
     </>
   );
 }
